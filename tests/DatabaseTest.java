305a8554054fcccd3238146f3772bdeae81fe50c
Flerxc <84610620+Flerxc@users.noreply.github.com>
2021-09-12 17:07:37 -0400

    Added tests for the database

---
+++ tests/DatabaseTest.java
@@ -0,0 +1,89 @@
+package tests;
+
+import org.junit.*;
+
+import data.Database;
+
+/**
+ * Tests for the Database class
+ * @author Francis Leroux-Contant
+ * @version 2021-09-12
+ */
+public class DatabaseTest {
+	
+	@Test
+	 public void itShouldConnectToADatabase() {
+		 // Arrange
+		Database  db =  new Database();
+		boolean connEstablished = false; 
+		
+		 // Act
+		 connEstablished = db.connect("test.db");
+				 
+		 // Assert
+		 Assert.assertTrue(connEstablished);
+	 }
+	
+	
+	@Test
+	 public void itShouldDisconnectFromADatabase() {
+		 // Arrange
+		Database  db =  new Database();
+		boolean disconnected = false; 
+		
+		 // Act
+		 db.connect("test.db");
+		 disconnected = db.disconnect();
+				 
+		 // Assert
+		 Assert.assertTrue(disconnected);
+	 }
+	
+	@Test
+	public void itShouldCreateATable() {
+		 // Arrange
+		Database  db =  new Database();
+		boolean tableCreated = false; 
+		
+		 // Act
+		 db.connect("test.db");
+		 
+		 tableCreated = db.createTable("test_ghost");
+		 
+		 db.disconnect();
+				 
+		 // Assert
+		 Assert.assertTrue(tableCreated);
+	}
+	
+	@Test
+	public void itShouldAddRightGhostToDatabase() {
+		 // Arrange
+		Database  db =  new Database();
+		db.connect("test.db");
+		boolean ghostAdded = false; 
+		
+		 // Act
+		ghostAdded = db.addGhost("Spirit", "test_game");
+		db.disconnect();
+				 
+		 // Assert
+		 Assert.assertTrue(ghostAdded);
+	}
+	
+	@Test
+	public void itShouldAddWrongGhostToDatabase() {
+		 // Arrange
+		Database  db =  new Database();
+		db.connect("test.db");
+		boolean ghostAdded = false; 
+		
+		 // Act
+		ghostAdded = db.addGhost("Spirit", "Banshee", "test_game");
+		db.disconnect();
+				 
+		 // Assert
+		 Assert.assertTrue(ghostAdded);
+		
+	}
+}
